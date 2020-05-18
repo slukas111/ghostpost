8aff78b93c1ab4bd4562f8c9eaf8b6d17e155bc2
Sasha Lukas <sashamati@gmail.com>
2020-05-18 13:58:23 -0700

    setup user and admin. directed to admin file. hashed out first post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+# [x]One model to represent both boasts and roasts
+# [x]Boolean to tell whether it's a boast or a roast
+# [x]CharField to put the content of the post in
+# [x]IntegerField for up votes
+# [x]IntegerField for down votes
+# [x]DateTimeField for submission time
+
+class GhostPost(models.Model):
+    total_both = models.IntegerField(default=0)
+    is_boast = models.BooleanField()
+    post = models.CharField(max_length=500)
+    up_votes= models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    time_post =models.DateTimeField(default=timezone.now)
+
